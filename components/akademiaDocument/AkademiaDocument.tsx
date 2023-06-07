7abc98c66eb7601d4c2a067ad0309f4b3d48506d
VoyTechCEO <makszokos@gmail.com>
2023-06-07 20:07:36 +0200

    set up tinymce;

--- components/akademiaDocument/AkademiaDocument.tsx
+++ components/akademiaDocument/AkademiaDocument.tsx
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import akademiaDocumentStyles from './akademiaDocument.module.css';
+import { Editor } from '@tinymce/tinymce-react';
+import { Editor as TinyMCEEditor } from 'tinymce';
 
 const AkademiaDocument = () => {
+  const editorRef = useRef<TinyMCEEditor | null>(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
+
   return (
     <>
       <div className={`container ${akademiaDocumentStyles.container}`}>
-        <h4>Kwiecień 2018</h4>
-        <h1>Komunikat 0</h1>
-        <p>
-          <b>Szanowni Państwo!</b>
-          <br />Z przyjemnością zawiadamiamy, że jesienią 2018 roku będzie
-          możliwość poznania teorii i sztuki chiropraktyki całego aparatu ruchu.
-        </p>
+        <Editor
+          id='EditorID'
+          tinymceScriptSrc={
+            process.env.NEXT_PUBLIC_PUBLIC_URL + '/tinymce/tinymce.min.js'
+          }
+          onInit={(evt, editor) => (editorRef.current = editor)}
+          initialValue='<p>This is the initial content of the editor.</p>'
+          init={{
+            menubar: false,
+            plugins: [
+              'advlist',
+              'autolink',
+              'lists',
+              'link',
+              'image',
+              'charmap',
+              'anchor',
+              'searchreplace',
+              'visualblocks',
+              'code',
+              'fullscreen',
+              'insertdatetime',
+              'media',
+              'table',
+              'preview',
+              'help',
+              'wordcount',
+            ],
+            toolbar:
+              'undo redo | blocks | ' +
+              'bold italic forecolor | alignleft aligncenter ' +
+              'alignright alignjustify | bullist numlist outdent indent | ' +
+              'removeformat | help',
+            content_style:
+              'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+          }}
+        />
+        <button onClick={log}>Log editor content</button>
       </div>
     </>
   );
