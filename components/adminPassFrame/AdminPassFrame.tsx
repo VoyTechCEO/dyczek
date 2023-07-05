0d9a8e24256799c5f43007dd332974aa61d0dd72
VoyTechCEO <makszokos@gmail.com>
2023-07-05 19:25:57 +0200

    test fixing posting problems (again);

--- components/adminPassFrame/AdminPassFrame.tsx
+++ components/adminPassFrame/AdminPassFrame.tsx
@@ -20,7 +20,7 @@ const AdminPassFrame = () => {
         }),
       });
       const data = await res.json();
-      console.log(data.response);
+      console.log(data);
       setLoginStatus(data.response.isPasswordCorrect);
       if (data.response.isPasswordCorrect) {
         router.reload();
