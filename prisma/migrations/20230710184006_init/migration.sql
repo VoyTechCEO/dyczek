7bcb34a75de3f9f324bc5c8a1b8b8baed0a67279
VoyTechCEO <makszokos@gmail.com>
2023-07-10 20:47:36 +0200

    change db to postgresql;

--- prisma/migrations/20230710184006_init/migration.sql
+++ prisma/migrations/20230710184006_init/migration.sql
@@ -1,22 +1,28 @@
 -- CreateTable
 CREATE TABLE "NoticeAdvanced" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "desc" TEXT NOT NULL,
     "date" TEXT NOT NULL,
-    "content" TEXT NOT NULL
+    "content" TEXT NOT NULL,
+
+    CONSTRAINT "NoticeAdvanced_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "NoticeBasic" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "date" TEXT NOT NULL,
-    "content" TEXT NOT NULL
+    "content" TEXT NOT NULL,
+
+    CONSTRAINT "NoticeBasic_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "GraduatesYear" (
-    "id" TEXT NOT NULL PRIMARY KEY,
     "year" INTEGER NOT NULL,
     "graduates" TEXT NOT NULL
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "GraduatesYear_year_key" ON "GraduatesYear"("year");
