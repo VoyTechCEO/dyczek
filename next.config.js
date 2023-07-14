6adb6adf12d7acfab76f96568b131bc0de395463
VoyTechCEO <makszokos@gmail.com>
2023-07-14 20:09:44 +0200

    analyze bundle size;

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 const { i18n } = require('./next-i18next.config');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withBundleAnalyzer({
   reactStrictMode: true,
   i18n,
-};
+});
 
 module.exports = nextConfig;
