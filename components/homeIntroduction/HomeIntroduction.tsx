1a75926740c8a792934848cdec561a0279878585
VoyTechCEO <makszokos@gmail.com>
2023-07-20 22:38:38 +0200

    add react spring; add changing profile picture for henryk dyczek page;

--- components/homeIntroduction/HomeIntroduction.tsx
+++ components/homeIntroduction/HomeIntroduction.tsx
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import homeIntroductionStyles from './homeIntroduction.module.css';
 
 const HomeIntroduction = () => {
+  const profileImages: string[] = [
+    '/img/dyczekHenryk.jpg',
+    '/img/dyczekHenryk1.jpg',
+    '/img/dyczekHenryk2.jpg',
+    '/img/dyczekHenryk3.jpg',
+  ];
+  const [currentImg, setCurrentImg] = useState(profileImages[0]);
+  const [previousImg, setPreviousImg] = useState(
+    profileImages[profileImages.length - 1]
+  );
+  const changeImage = () => {
+    setPreviousImg((previousImg) => {
+      const previous = profileImages.indexOf(previousImg);
+      if (previous === profileImages.length - 1) {
+        return profileImages[0];
+      } else {
+        return profileImages[previous + 1];
+      }
+    });
+    setCurrentImg((currentImg) => {
+      const current = profileImages.indexOf(currentImg);
+      if (current === profileImages.length - 1) {
+        return profileImages[0];
+      } else {
+        return profileImages[current + 1];
+      }
+    });
+  };
+
+  useEffect(() => {
+    const interval = setInterval(changeImage, 7000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <>
       <article className={`container ${homeIntroductionStyles.container}`}>
-        <img src='/img/dyczekHenryk.jpg' alt='Dyczek Henryk' />
+        <div className={homeIntroductionStyles.profiles}>
+          {profileImages.map((item, index) => {
+            return (
+              <img
+                key={`${item}homeProfilePic${index}`}
+                className={`${
+                  currentImg === item
+                    ? homeIntroductionStyles.visible
+                    : previousImg === item
+                    ? homeIntroductionStyles.previous
+                    : homeIntroductionStyles.invisible
+                }`}
+                src={item}
+                alt='Dyczek Henryk'
+              />
+            );
+          })}
+        </div>
         <ul>
           <li>
             dr n. med. <span>HENRYK DYCZEK</span>, D.C. (Diploma in
