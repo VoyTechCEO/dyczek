d73ab94e442a23bd29a6f0ebcbfde84208050ddd
VoyTechCEO <makszokos@gmail.com>
2023-07-28 21:26:49 +0200

    add scrolling effect on akademiaCh;

--- components/akademiaMotto/AkademiaMotto.tsx
+++ components/akademiaMotto/AkademiaMotto.tsx
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import akademiaMottoStyles from './akademiaMotto.module.css';
 import { useTranslation } from 'next-i18next';
 import ElementRef from '../elementRef/ElementRef';
+import { useScroll, animated } from '@react-spring/web';
 
 const AkademiaMotto = () => {
   const { t } = useTranslation();
+  const [progress, setProgress] = useState(0);
+  const ref = useRef<HTMLDivElement>(null!);
+
+  // animations
+  const { scrollYProgress } = useScroll({
+    onChange: ({ value: { scrollYProgress } }) => {
+      // if (
+      //   ref.current.getBoundingClientRect().top < 600 &&
+      //   ref.current.getBoundingClientRect().top > 350
+      // ) {
+      //   setProgress(scrollYProgress * 5);
+      // }
+      // console.log(`top: ${ref.current.getBoundingClientRect().top}`);
+      if (document.body.scrollHeight > 3800) {
+        setProgress(scrollYProgress * 20);
+      } else if (document.body.scrollHeight > 3300) {
+        setProgress(scrollYProgress * 15);
+      } else if (document.body.scrollHeight > 3100) {
+        setProgress(scrollYProgress * 13);
+      } else {
+        setProgress(scrollYProgress * 8);
+      }
+      console.log(document.body.scrollHeight);
+    },
+  });
 
   return (
     <>
-      <div className={`container ${akademiaMottoStyles.container}`}>
+      <animated.div
+        className={`container ${akademiaMottoStyles.container}`}
+        style={{ opacity: progress }}
+        ref={ref}
+      >
         <div className={akademiaMottoStyles.edge} />
         <ElementRef element='h1' content={t('akademiaChMain:motto')} />
         <div className={akademiaMottoStyles.edge} />
-      </div>
+      </animated.div>
     </>
   );
 };
