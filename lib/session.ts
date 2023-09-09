d86598181010fad8732d2c9f0c6da822aca051b1
VoyTechCEO <makszokos@gmail.com>
2023-09-09 19:49:22 +0200

    change domain in cookie options;

--- lib/session.ts
+++ lib/session.ts
@@ -5,7 +5,7 @@ const sessionOptions: IronSessionOptions = {
   password: process.env.SECRET_COOKIE_PASSWORD as string,
   cookieName: 'loginCookie',
   cookieOptions: {
-    domain: 'https://dyczekpl.cfolks.pl/',
+    domain: 'https://dyczekpl.cfolks.pl',
     sameSite: 'lax',
     secure: process.env.NODE_ENV === 'production',
     maxAge: 21600,
