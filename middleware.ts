48f4a3be021f2553048d6805568f5797e7e446b8
VoyTechCEO <makszokos@gmail.com>
2023-09-13 16:57:35 +0200

    back to default next.js cors;

--- middleware.ts
+++ middleware.ts
@@ -1,38 +1,38 @@
 import { NextResponse } from 'next/server';
 
-const allowedOrigins =
-  process.env.NODE_ENV === 'production'
-    ? [
-        'https://dyczek.netlify.app',
-        'https://dyczekpl.cfolks.pl',
-        'http://localhost:3000',
-      ]
-    : ['http://localhost:3000'];
+// const allowedOrigins =
+//   process.env.NODE_ENV === 'production'
+//     ? [
+//         'https://dyczek.netlify.app',
+//         'https://dyczekpl.cfolks.pl',
+//         'http://localhost:3000',
+//       ]
+//     : ['http://localhost:3000'];
 
 export function middleware(request: Request) {
-  const origin = request.headers.get('origin');
+  // const origin = request.headers.get('origin');
 
-  if (origin && !allowedOrigins.includes(origin)) {
-    return new NextResponse(null, {
-      status: 400,
-      statusText: 'Bad request',
-      headers: {
-        'Content-Type': 'text/plain',
-      },
-    });
-  }
+  // if (origin && !allowedOrigins.includes(origin)) {
+  //   return new NextResponse(null, {
+  //     status: 400,
+  //     statusText: 'Bad request',
+  //     headers: {
+  //       'Content-Type': 'text/plain',
+  //     },
+  //   });
+  // }
   const res = NextResponse.next();
-  res.headers.append('Access-Control-Allow-Credentials', 'true');
-  res.headers.append('Access-Control-Allow-Origin', origin || '');
-  res.headers.append(
-    'Access-Control-Allow-Methods',
-    'GET,DELETE,PATCH,POST,PUT,OPTIONS'
-  );
-  res.headers.append('Access-Control-Allow-Headers', '*');
+  // res.headers.append('Access-Control-Allow-Credentials', 'true');
+  // res.headers.append('Access-Control-Allow-Origin', origin || '');
+  // res.headers.append(
+  //   'Access-Control-Allow-Methods',
+  //   'GET,DELETE,PATCH,POST,PUT,OPTIONS'
+  // );
+  // res.headers.append('Access-Control-Allow-Headers', '*');
 
   return res;
 }
 
-export const config = {
-  matcher: '/api/:path*',
-};
+// export const config = {
+//   matcher: '/api/:path*',
+// };
